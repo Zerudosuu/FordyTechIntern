5aebf62404db67cf53389ff56e5ad7f96392a4bc
Ronald Salvador <140569363+Zerudosuu@users.noreply.github.com>
2024-11-02 17:46:10 +0800

    Added blockings

---
+++ todoapp/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+  return (
+    <footer className="w-full h-[10%] border-4 border-black">Footer</footer>
+  );
+};
+
+export default Footer;
---
+++ todoapp/src/components/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <nav className=" w-full h-[10%] border-4 border-black">Header</nav>;
+};
+
+export default Header;
